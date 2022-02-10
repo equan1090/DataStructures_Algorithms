a71773874edc93dfd613ee859cfda7c9e82690ac
Eric Quan <equan1090@gmail.com>
2022-02-09 17:30:13 -0700

    completed

---
+++ python/validateSubsequence/validateSubsequence.py
@@ -0,0 +1,15 @@
+# Given two non-empty binary arrays of integers, write a function that determines whether the second array
+# is a subsequence of the first one.
+# A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but are in the same order
+# as they appear in the array
+
+def isValidSubsequence(array, sequence):
+    sqxIdx = 0
+    for val in array:
+        if sqxIdx == len(sequence):
+            break
+        if val == sequence[sqxIdx]:
+            sqxIdx += 1
+
+    return len(sequence) == sqxIdx
+
