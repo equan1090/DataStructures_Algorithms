1afeeaa9e84cf08fee9a9afb8da9ea379d161feb
Eric Quan <equan1090@gmail.com>
2022-02-09 17:26:28 -0700

    two sum completed

---
+++ python/twoSum/twoSum.py
@@ -0,0 +1,13 @@
+# Write a function that takes in a non-empy array of distince integers and an integer representing a target sum.
+# If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order.
+# if no two numbers sum up to the target sum, the function should return an empty array
+
+def twoNumberSum(array, targetSum):
+    my_dict = {}
+
+    for val in array:
+        compliment = targetSum - val
+        if compliment in my_dict:
+            return [compliment, val]
+        my_dict[val] = True
+    return []
