61ca53399ac65ed371e6ab7b8ba59de9e39121f0
Eric Quan <equan1090@gmail.com>
2022-02-15 10:56:01 -0700

    branch sums

---
+++ python/branchSums/branchSums.py
@@ -0,0 +1,25 @@
+# Write a function taht takes in a Binary Tree and returns a list of its branch sums ordered from the leftmost branch
+# sum to rightmost branch sum
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def branchSums(root):
+    sums = []
+    calculateBranchSums(root, 0, sums)
+    return sums
+
+def calculateBranchSums(node, runningSum, sums):
+    if node is None:
+        return
+
+    newRunningSum = runningSum + node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+    calculateBranchSums(node.left, newRunningSum, sums)
+    calculateBranchSums(node.right, newRunningSum, sums)
+
