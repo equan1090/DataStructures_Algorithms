960ceff8244bc13a3f375d558fff8c06ef3cbab2
Eric Quan <equan1090@gmail.com>
2022-02-21 16:09:55 -0700

    reverse linked list

---
+++ python/reverseLinkedList.py
@@ -0,0 +1,18 @@
+# def reverse_list(head):
+#     prev = None
+#     current = head
+#     while current is not None:
+#         next = current.next
+#         current.next = prev
+#         prev = current
+#         current = next
+#     return prev
+
+#Recursive approach
+def reverse_list(head, prev=None):
+    if head is None:
+        return prev
+    next = head.next
+    head.next = prev
+
+    return reverse_list(next, head)
