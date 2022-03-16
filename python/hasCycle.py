1693f3486c6e5edf13efbc1b3c5ebd9a2a6a5664
Eric Quan <equan1090@gmail.com>
2022-03-15 19:20:11 -0600

    all graph problems

---
+++ python/hasCycle.py
@@ -0,0 +1,30 @@
+'''
+Write a function, has_cycle, that takes in an object representing the adjacency list of a directed graph.
+ The function should return a boolean indicating whether or not the graph contains a cycle.
+'''
+
+
+def has_cycle(graph):
+    visited = set()
+    for start_node in graph:
+        if cycle_detect(graph, start_node, set(), visited):
+            return True
+    return False
+
+
+def cycle_detect(graph, node, visiting, visited):
+    if node in visited:
+        return False
+
+    if node in visiting:
+        return True
+
+    visiting.add(node)
+
+    for neighbor in graph[node]:
+        if cycle_detect(graph, neighbor, visiting, visited):
+            return True
+
+    visiting.remove(node)
+    visited.add(node)
+    return False
