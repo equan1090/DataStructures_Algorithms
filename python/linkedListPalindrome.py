ada013221630e6741393c4e150e36e301bd9141d
Eric Quan <equan1090@gmail.com>
2022-03-21 19:19:21 -0600

    added buy sell

---
+++ python/linkedListPalindrome.py
@@ -0,0 +1,35 @@
+
+'''
+Given the head of a singly linked list, return true if it is a palindrome.
+'''
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def isPalindrome(head):
+    slow = fast = head
+    # Get the middle of the linked list
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    # reverse
+    prev = None
+    while slow:
+        nxt = slow.next
+        slow.next = prev
+        prev = slow
+        slow = nxt
+
+    left, right = head, prev
+    while right:
+        if left.val != right.val:
+            return False
+        left = left.next
+        right = right.next
+
+    return True
