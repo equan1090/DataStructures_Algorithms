de07c81607376042ad9c9d79e231d6560188960b
Eric Quan <equan1090@gmail.com>
2022-03-26 09:46:34 -0600

    hascycle

--- python/hasCycle.py
+++ python/hasCycle.py
@@ -1,30 +1,52 @@
 '''
 Write a function, has_cycle, that takes in an object representing the adjacency list of a directed graph.
  The function should return a boolean indicating whether or not the graph contains a cycle.
+ 
+has_cycle({
+  "a": ["b"],
+  "b": ["c"],
+  "c": ["a"],
+}) # -> True
 '''
 
 
 def has_cycle(graph):
+    visiting = set()
     visited = set()
-    for start_node in graph:
-        if cycle_detect(graph, start_node, set(), visited):
+    for node in graph:
+        if _has_cycle(graph, node, visited, visiting):
             return True
     return False
 
-
-def cycle_detect(graph, node, visiting, visited):
-    if node in visited:
-        return False
-
+def _has_cycle(graph, node, visited, visiting):
     if node in visiting:
         return True
-
+    if node in visited:
+        return False
+    
     visiting.add(node)
-
     for neighbor in graph[node]:
-        if cycle_detect(graph, neighbor, visiting, visited):
+        if _has_cycle(graph, neighbor, visited, visiting):
             return True
-
+    
     visiting.remove(node)
     visited.add(node)
     return False
+
+
+print(has_cycle({
+    "a": ["b"],
+    "b": ["c"],
+    "c": ["a"],
+}))  # -> True
+
+print(has_cycle({
+    "q": ["r", "s"],
+    "r": ["t", "u"],
+    "s": [],
+    "t": [],
+    "u": [],
+    "v": ["w"],
+    "w": [],
+    "x": ["w"],
+}))  # -> False)
