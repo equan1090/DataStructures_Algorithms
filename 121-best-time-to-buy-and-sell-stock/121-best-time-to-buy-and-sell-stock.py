38f7d59fbb20f329d27723d9f2a7bc7ae36242c3
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-12 16:18:51 -0600

    Time: 1097 ms (83.92%), Space: 25 MB (39.60%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -6,7 +6,7 @@ class Solution:
         for r in range(len(prices)):
             curProfit = prices[r] - prices[l]
             if curProfit < 0:
-                curProfit = 0
+                
                 l = r
             res = max(res, curProfit)
         return res
