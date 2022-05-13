ba31e1d04c33b8d098aebdb7bff9c00b98faab36
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-12 20:58:03 -0600

    Time: 493 ms (28.83%), Space: 21.9 MB (21.45%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if self.traverse(grid, r, c, visited):
+                    count += 1
+        return count
+    
+    def traverse(self, grid, r, c, visited):
+        rowbounds = 0 <= r < len(grid)
+        colbounds = 0 <= c < len(grid[0])
+        
+        if not rowbounds or not colbounds or grid[r][c] == '0':
+            return False
+        
+        pos = (r, c)
+        if pos in visited:
+            return False
+        visited.add(pos)
+        
+        self.traverse(grid, r + 1, c, visited)
+        self.traverse(grid, r - 1, c, visited)
+        self.traverse(grid, r, c + 1, visited)
+        self.traverse(grid, r, c - 1, visited)
+        return True
+        
