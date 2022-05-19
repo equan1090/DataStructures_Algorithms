4c22f6db164e651bb124ad173880a923e887e9a7
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-18 19:33:31 -0600

    Time: 235 ms (99.34%), Space: 14.2 MB (91.93%) - LeetHub

--- 839-similar-string-groups/839-similar-string-groups.py
+++ 839-similar-string-groups/839-similar-string-groups.py
@@ -1,8 +1,8 @@
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
         D = {}
-        for i in strs:
-            D[i]=[i]
+        for word in strs:
+            D[word]=[word]
         n = len(strs[0])
         for i in range(len(strs)):
             for j in range(i+1,len(strs)):
@@ -16,16 +16,32 @@ class Solution:
                     D[strs[i]].append(strs[j])
                     D[strs[j]].append(strs[i])
         visited = set()
-        comp=0
-        for i in D.keys():
-            if i not in visited:
-                comp+=1
-                visited.add(i)
-                stack = [i]
-                while stack:
-                    cur = stack.pop()
-                    for new in D[cur]:
-                        if new not in visited:
-                            visited.add(new)
-                            stack.append(new)
-        return comp
+        count = 0
+        for node in D:
+            if self.dfs(D, node, visited):
+                count += 1
+        return count
+    
+    
+    def dfs(self, graph, node, visited):
+        if node in visited:
+            return False
+        visited.add(node)
+        
+        for neighbor in graph[node]:
+            self.dfs(graph, neighbor, visited)
+        return True
+        
+        # comp=0
+        # for i in D.keys():
+        #     if i not in visited:
+        #         comp+=1
+        #         visited.add(i)
+        #         stack = [i]
+        #         while stack:
+        #             cur = stack.pop()
+        #             for new in D[cur]:
+        #                 if new not in visited:
+        #                     visited.add(new)
+        #                     stack.append(new)
+        # return comp
