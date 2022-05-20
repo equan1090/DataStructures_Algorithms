f765d2112c601e2600d7c010fb0122eaa3ee4cc2
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-20 15:25:22 -0600

    Time: 31 ms (84.56%), Space: 13.8 MB (51.91%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.py
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -1,3 +1,12 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
-        return num1 + num2
+
+        mask = 0xffffffff
+        while num2 != 0:
+            tmp = (num1 & num2) << 1
+            num1 = (num1 ^ num2) & mask
+            num2 = tmp & mask
+        if num1 > mask // 2:
+            return ~(num1 ^ mask)
+        else:
+            return num1
