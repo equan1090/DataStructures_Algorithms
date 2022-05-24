73e64d8c36516ce00c1c1b153acab54a1e2ad88f
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-24 15:24:28 -0600

    Time: 106 ms (71.58%), Space: 14.1 MB (68.33%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,30 +1,37 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
+        short = nums1
+        long = nums2
+        total = (len(nums1) + len(nums2))
         half = total // 2
         
-        if len(B) < len(A):
-            A, B = B, A
-            
+        if len(short) > len(long):
+            short, long = long, short
+        
         l = 0
-        r = len(A) - 1
+        r = len(short) - 1
         while True:
             i = (l + r) // 2
             j = half - i - 2
             
-            Aleft = A[i] if i >= 0 else float('-inf')
-            Aright = A[i + 1] if (i+1) < len(A) else float('inf')
-            Bleft = B[j] if j >= 0 else float('-inf')
-            Bright = B[j + 1] if (j + 1) < len(B) else float('inf')
+            shortLeft = short[i] if i >= 0 else float('-inf')
+            shortRight = short[i + 1] if (i + 1) < len(short) else float('inf')
+            longLeft = long[j] if j >= 0 else float('-inf')
+            longRight = long[j + 1] if (j + 1) < len(long) else float('inf')
             
-            if Aleft <= Bright and Bleft <= Aright:
+            if shortLeft <= longRight and longLeft <= shortRight:
                 
                 if total % 2:
-                    return min(Aright, Bright)
+                    return min(shortRight, longRight)
                 
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
+                return (max(shortLeft, longLeft) + min(shortRight, longRight)) / 2
+            
+            elif shortLeft > longRight:
                 r = i - 1
             else:
                 l = i + 1
+                
+
+            
+            
+            
