bc48a8fee12e1a3bde950c44e04d3f8578df184a
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-25 12:54:19 -0600

    Time: 159 ms (34.36%), Space: 19.6 MB (11.00%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #bucket = [[][3][2][1][][][]]
         bucket = [[] for i in range(len(nums) + 1)]
         freq = {}
         
@@ -8,13 +9,16 @@ class Solution:
                 freq[num] = 0
             freq[num] += 1
         
+        # freq = {1: 3, 2: 2, 3: 1}
         for val, idx in freq.items():
             bucket[idx].append(val)
         
         res = []
-        for i in range(len(bucket) -1, -1, -1):
+        
+        for i in range(len(bucket) - 1, - 1, -1):
             for val in bucket[i]:
                 res.append(val)
                 if len(res) == k:
                     return res
-                
+        return []
+            
