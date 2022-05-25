ba1681a0b3ef76e0a63d438d4138f1f0ced16385
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-25 10:15:31 -0600

    Time: 1214 ms (59.28%), Space: 13.9 MB (91.79%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -9,10 +9,10 @@ class Solution:
             
             l = i
             r = i
-            
+            i = r + 1
             while r < len(s) - 1 and s[r] == s[r+1]:
                 r += 1
-            i = r + 1
+            
             while r < len(s) - 1 and l > 0 and s[r+1] == s[l-1]:
                 l -= 1
                 r += 1
