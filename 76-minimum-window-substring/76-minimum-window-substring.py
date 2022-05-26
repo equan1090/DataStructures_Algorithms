389c3803f36d8a70c40e5906245d8f771df7b146
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-26 16:29:50 -0600

    Time: 140 ms (57.98%), Space: 14.7 MB (83.26%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,52 +1,39 @@
-# class Solution:
-#     def minWindow(self, s: str, t: str) -> str:
-#         l = 0
-#         resLen = float('inf')
-#         res = [-1, -1]
-#         tCount = {}
-#         window = {}
-#         have = 0
-#         need = len(t)
-#         for c in t:
-#             tCount[c] = 1 + tCount.get(c, 0)
-        
-#         for r in range(len(s)):
-#             c = s[r]
-#             window[c] = 1 + window.get(c, 0)
-            
-#             if c in window:
-#                 have += 1
-            
-#             while have == need:
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "": return ""
+        if t == '':
+            return ''
         
-        countT, window = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
-        
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
         l = 0
+        resLen = float('inf')
+        res = [-1, -1]
+        tCount = {}
+        window = {}
+
+        for c in t:
+            tCount[c] = 1 + tCount.get(c, 0)
+            
+        have, need = 0, len(tCount)
         for r in range(len(s)):
             c = s[r]
             window[c] = 1 + window.get(c, 0)
             
-            if c in countT and window[c] == countT[c]:
+            if c in tCount and window[c] == tCount[c]:
                 have += 1
-        
+                
             while have == need:
-                # update our result
+                
+                
                 if (r - l + 1) < resLen:
-                    res = [l, r]
                     resLen = (r - l + 1)
-                # pop from the left of our window
+                    res = [l, r]
+                
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if s[l] in tCount and window[s[l]] < tCount[s[l]]:
                     have -= 1
                 l += 1
         l, r = res
-        return s[l:r+1]
+        return s[l:r + 1]
                 
+            
+
             
