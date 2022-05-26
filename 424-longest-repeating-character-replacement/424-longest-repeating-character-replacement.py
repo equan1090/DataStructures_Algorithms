462e3767b0d535105624e2f31db4fe1c92975c86
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-26 13:08:36 -0600

    Time: 141 ms (77.42%), Space: 14 MB (18.78%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,8 +1,8 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
         freq = {}
         l = 0
-        res = 0
         for r in range(len(s)):
             if s[r] not in freq:
                 freq[s[r]] = 0
@@ -11,5 +11,6 @@ class Solution:
             if (r - l + 1) - max(freq.values()) > k:
                 freq[s[l]] -= 1
                 l += 1
+            
             res = max(res, r - l + 1)
         return res
