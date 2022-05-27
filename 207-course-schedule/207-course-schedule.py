9a753c3bd14a14f9e59ec4af157a49db92abf120
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-27 12:11:43 -0600

    Time: 136 ms (48.97%), Space: 16.9 MB (50.30%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,23 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {i:[] for i in range(numCourses)}
-        for crs, pre in prerequisites:
-            graph[crs].append(pre)
+        
+        for a, b in prerequisites:
+            graph[a].append(b)
         
         visited = set()
         visiting = set()
+        
         for node in graph:
             if self.hasCycle(graph, node, visited, visiting):
                 return False
         return True
         
     def hasCycle(self, graph, node, visited, visiting):
-        if node in visiting:
-            return True
         if node in visited:
             return False
+        if node in visiting:
+            return True
         
         visiting.add(node)
+        
         for neighbor in graph[node]:
             if self.hasCycle(graph, neighbor, visited, visiting):
                 return True
