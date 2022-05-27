45f284b864d1a01b3ba4f9fbb6212a8b9dcf706f
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-26 22:05:58 -0600

    Time: 86 ms (96.81%), Space: 14.7 MB (83.26%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,19 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if t == '':
+            return ''
         window = {}
         tCount = {}
         for c in t:
             tCount[c] = 1 + tCount.get(c, 0)
+        
         have = 0
         need = len(tCount)
         l = 0
         res = [-1, -1]
         resLen = float('inf')
-        
         for r in range(len(s)):
             c = s[r]
             window[c] = 1 + window.get(c, 0)
-            
             if c in tCount and window[c] == tCount[c]:
                 have += 1
             
@@ -24,9 +25,9 @@ class Solution:
                     res = [l, r]
                 
                 window[s[l]] -= 1
-                if s[l] in tCount and window[s[l]] < tCount [s[l]]:
+                if s[l] in tCount and window[s[l]] < tCount[s[l]]:
                     have -= 1
+                
                 l += 1
         l, r = res
         return s[l:r + 1]
-        
