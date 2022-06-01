2401273918ef4cfb335b0989027ff00689e7ad75
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-01 13:10:02 -0600

    Time: 1093 ms (20.71%), Space: 21.5 MB (36.31%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,11 +1,9 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        visited = set()
-        
-        #Keep track or row, column, distance, and demolition charges
         q = deque([(0, 0, 0, k)])
-        
+        visited = set((0, 0, 0))
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
         while q:
             r, c, distance, demo = q.popleft()
             
@@ -13,17 +11,16 @@ class Solution:
                 return distance
             
             for dr, dc in directions:
-                nRow = r + dr
-                nCol = c + dc
+                nRow = dr + r
+                nCol = dc + c
                 rowbounds = 0 <= nRow < len(grid)
                 colbounds = 0 <= nCol < len(grid[0])
                 pos = (nRow, nCol, demo)
+                
                 if rowbounds and colbounds and pos not in visited:
-                    
+                    visited.add(pos)
                     if grid[nRow][nCol] == 1 and demo > 0:
-                        visited.add(pos)
                         q.append((nRow, nCol, distance + 1, demo - 1))
-                    elif grid[nRow][nCol] == 0:
-                        visited.add(pos)
+                    if grid[nRow][nCol] == 0:
                         q.append((nRow, nCol, distance + 1, demo))
         return -1
