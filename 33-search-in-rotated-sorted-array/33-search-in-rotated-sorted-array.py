35bf92d3c61614eb9f1e40d31f7462519c3972ca
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-05-31 19:42:28 -0600

    Time: 55 ms (53.15%), Space: 14.3 MB (18.45%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -5,9 +5,10 @@ class Solution:
         
         while l <= r:
             mid = (l + r) // 2
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
             
+            # left sorted portion
             if nums[l] <= nums[mid]:
                 if target > nums[mid] or target < nums[l]:
                     l = mid + 1
@@ -19,3 +20,7 @@ class Solution:
                 else:
                     l = mid + 1
         return -1
+                
+                
+                
+                
