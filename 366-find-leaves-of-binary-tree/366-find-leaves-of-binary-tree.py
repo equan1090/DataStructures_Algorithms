23c75ca71f835fbc93d234c050935a808ddce11b
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-01 12:45:49 -0600

    Time: 51 ms (28.82%), Space: 13.8 MB (73.56%) - LeetHub

---
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        self._findLeaves(root, res)
+        return res
+    
+    def _findLeaves(self, root, res):
+        if root is None:
+            return 0
+        
+        left = self._findLeaves(root.left, res)
+        right = self._findLeaves(root.right, res)
+        
+        level = max(left, right)
+        
+        if len(res) == level:
+            res.append([])
+        res[level].append(root.val)
+        return level + 1
+    
+        
