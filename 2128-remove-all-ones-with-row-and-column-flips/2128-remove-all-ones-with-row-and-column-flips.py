456ff6cf53032e8d7e4ad13db4db934aff25d45b
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-03 14:53:17 -0600

    Time: 568 ms (49.05%), Space: 16.5 MB (51.03%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.py
@@ -1,9 +1,9 @@
 class Solution:
     def removeOnes(self, grid: List[List[int]]) -> bool:
         r1 = grid[0]
-        invert = [1 - val for val in grid[0]]
+        inverse = [1 - val for val in grid[0]]
         
         for i in range(1, len(grid)):
-            if r1 != grid[i] and invert != grid[i]:
+            if r1 != grid[i] and inverse != grid[i]:
                 return False
         return True
