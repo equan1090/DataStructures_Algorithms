43a307e5bfe79be4bb546308d51a5762c27fcd68
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-03 13:06:44 -0600

    Time: 76 ms (58.87%), Space: 15.9 MB (43.53%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -1,6 +1,7 @@
 class Solution:
     def racecar(self, target: int) -> int:
         q = deque([(0, 0, 1)])
+        visited = set((0, 1))
         
         while q:
             moves, pos, speed = q.popleft()
@@ -8,8 +9,11 @@ class Solution:
             if pos == target:
                 return moves
             
-            q.append((moves + 1, pos + speed, speed * 2))
-            
-            if (pos + speed > target and speed > 0) or (pos + speed < target and speed < 0):
-                speed = 1 if speed < 0 else -1
-                q.append((moves + 1, pos, speed))
+            if (pos, speed) not in visited:
+                visited.add((pos, speed))
+                
+                q.append((moves + 1, pos + speed, speed * 2))
+
+                if (pos + speed > target and speed > 0) or (pos + speed < target and speed < 0):
+                    speed = 1 if speed < 0 else -1
+                    q.append((moves + 1, pos, speed))
