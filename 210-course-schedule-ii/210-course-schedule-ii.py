50b0956872bdcaad49f7d1501ca7aabdf8282436
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-03 17:04:11 -0600

    Time: 115 ms (69.37%), Space: 17 MB (44.72%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {i:[] for i in range(numCourses)}
+        
+        for a, b in prerequisites:
+            graph[a].append(b)
+        
+        visited = set()
+        visiting = set()
+        res = []
+        
+        for node in graph:
+            if self.hasCycle(graph, node, visited, visiting, res):
+                return []
+        
+        return res
+        
+        
+    def hasCycle(self, graph, node, visited, visiting, res):
+        if node in visited:
+            return False
+        
+        if node in visiting:
+            return True
+        
+        visiting.add(node)
+        
+        for neighbor in graph[node]:
+            if self.hasCycle(graph, neighbor, visited, visiting, res):
+                return True
+        
+        visiting.remove(node)
+        visited.add(node)
+        res.append(node)
+        return False
+                
+        
+        
