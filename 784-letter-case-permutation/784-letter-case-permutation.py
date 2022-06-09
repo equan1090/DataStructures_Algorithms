f483a23093eca218c89d3ca6c81b12bd3d44ad56
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-09 14:26:41 -0600

    Time: 72 ms (66.61%), Space: 14.6 MB (79.93%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,22 +1,19 @@
-from collections import deque
-class Solution:
-    def letterCasePermutation(self, s: str) -> List[str]:
-        res = []
-        
-        self.permute(s, [], 0, res)
-        return res
-        
-    def permute(self, s, path, i, res):
-        if len(path) == len(s):
-            res.append(''.join(path))
-            return
-        
-        self.permute(s, path + [s[i]], i + 1, res)
-        
-        if s[i].isalpha():
-            self.permute(s, path + [s[i].swapcase()], i + 1, res)
-        
-        
-        
-            
-            
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        digits = {str(x) for x in range(10)}
+        A = ['']
+        for c in S:
+            B = []
+            if c in digits:
+                for a in A:
+                    B.append(a+c)
+            else:
+                for a in A:
+                    B.append(a+c.lower())
+                    B.append(a+c.upper())
+            A=B
+        return A
