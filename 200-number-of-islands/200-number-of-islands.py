346b8825800d9ed000a2da92f75f29057b04f7fb
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-09 16:53:08 -0600

    Time: 367 ms (68.81%), Space: 21.9 MB (24.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,26 +4,28 @@ class Solution:
         count = 0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if self.traverse(grid, r, c, visited):
+                if self.dfs(grid, r, c, visited):
                     count += 1
         return count
         
-        
-    def traverse(self, grid, r, c, visited):
+    def dfs(self, grid, r, c, visited):
         rowbounds = 0 <= r < len(grid)
         colbounds = 0 <= c < len(grid[0])
-        
-        if not rowbounds or not colbounds or grid[r][c] == '0':
+        if not rowbounds or not colbounds:
             return False
         
-        if (r, c) in visited:
+        if grid[r][c] == '0':
             return False
         
-        visited.add((r, c))
+        pos = (r, c)
+        if pos in visited:
+            return False
         
-        self.traverse(grid, r + 1, c, visited)
-        self.traverse(grid, r - 1, c, visited)
-        self.traverse(grid, r, c + 1, visited)
-        self.traverse(grid, r, c - 1, visited)
+        visited.add(pos)
+        
+        self.dfs(grid, r + 1, c, visited)
+        self.dfs(grid, r - 1, c, visited)
+        self.dfs(grid, r, c + 1, visited)
+        self.dfs(grid, r, c - 1, visited)
         
         return True
