1b907a4e9c08555a7c1fb6bab12ed617c81bccad
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-10 00:44:44 -0600

    Time: 105 ms (28.49%), Space: 15.9 MB (20.12%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -8,25 +8,18 @@ class Node:
         self.next = next
 """
 
-
 class Solution:
-    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        n_0, n_1 = root, root.left if root else None
-        # n_0 is our pointer to the current layer
-        # n_1 is our pointer to next layer
-        # We use n_0 to connect all the nodes belonging to next layer, meaning the layer n_1 belongs to.
+    def connect(self, node: 'Optional[Node]') -> 'Optional[Node]':
+        cur = node
+        nxt = node.left if node else None
         
-        while n_1: # while there is a next layer to connect, we continue
+        while cur and nxt:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
             
-            n_0.left.next = n_0.right # 1st action we can always do if there is a next layer after n_0
-            
-            if n_0.next: # 2nd action we might be able to do if we are not all the way to the right
-                
-                n_0.right.next = n_0.next.left
-                
-                n_0 = n_0.next 
-                
-                continue # we keep going to the "right" until the layer is all connected
-                
-            n_0, n_1  = n_1, n_1.left # "Recursive call" to handle the next layer
-        return root
+            cur = cur.next
+            if not cur:
+                cur = nxt
+                nxt = cur.left
+        return node
