cdf896cbeb75b741bcfcb0318984b58dd1e4c3d9
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-13 17:02:01 -0600

    Time: 943 ms (28.33%), Space: 21.3 MB (40.86%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,9 +1,8 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         q = deque([(0, 0, 0, k)])
-        visited = set((0, 0, 0))
+        visited = set((0, 0, k))
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        
         while q:
             r, c, distance, demo = q.popleft()
             
@@ -13,14 +12,17 @@ class Solution:
             for dr, dc in directions:
                 nRow = dr + r
                 nCol = dc + c
+                pos = (nRow, nCol, demo)
                 rowbounds = 0 <= nRow < len(grid)
                 colbounds = 0 <= nCol < len(grid[0])
-                pos = (nRow, nCol, demo)
                 
                 if rowbounds and colbounds and pos not in visited:
                     visited.add(pos)
                     if grid[nRow][nCol] == 1 and demo > 0:
                         q.append((nRow, nCol, distance + 1, demo - 1))
+                        
                     if grid[nRow][nCol] == 0:
                         q.append((nRow, nCol, distance + 1, demo))
+                
+            
         return -1
