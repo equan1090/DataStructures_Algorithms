f78178fefeb51523bd4c0bcc6fc14fbdaa2f32a6
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-13 16:31:30 -0600

    Time: 59 ms (32.25%), Space: 14.1 MB (75.91%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        self.dfs(n, n, [], res)
+        return res
+    
+    def dfs(self, opened, closed, path, res):
+        if opened > closed or opened < 0 or closed < 0:
+            return
+        
+        if opened == 0 and closed == 0:
+            res.append(''.join(path))
+            return
+        
+        self.dfs(opened - 1, closed, path + ['('], res)
+        self.dfs(opened, closed - 1, path + [')'], res)
+        return res
+        
+        
