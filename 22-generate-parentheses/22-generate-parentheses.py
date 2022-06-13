eef17ec09519c06cec90796867b9424a26f78509
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-13 16:48:30 -0600

    Time: 80 ms (9.28%), Space: 14.2 MB (38.86%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -5,15 +5,14 @@ class Solution:
         return res
     
     def dfs(self, opened, closed, path, res):
+        # if we ever have more opened than closes remaining, then that means we added a closed before an open parentheses
+        # example we start with 3 opened, 3 closed, if we end up inputting '())', that means we have 2 opened left with 1 closed left, 3 - 1 vs 3 - 2
         if opened > closed or opened < 0 or closed < 0:
             return
         
         if opened == 0 and closed == 0:
             res.append(''.join(path))
-            return
         
         self.dfs(opened - 1, closed, path + ['('], res)
         self.dfs(opened, closed - 1, path + [')'], res)
         return res
-        
-        
