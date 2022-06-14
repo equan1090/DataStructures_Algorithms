80c80eee7f5568eced8a54dbdfd4fbc84e74db8b
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-13 18:51:27 -0600

    Time: 3229 ms (46.90%), Space: 47.8 MB (72.93%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.py
@@ -1,26 +1,24 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        m, n = len(points), len(points[0])
-        
+        M, N = len(points), len(points[0])
+        left = [0] * N
+        right = [0] * N
         dp = points[0]
-        
-        left = [0] * n ## left side contribution
-        right = [0] * n ## right side contribution
-        
-        for r in range(1, m):
-            for c in range(n):
-                if c == 0:
-                    left[c] = dp[c]
+        for i in range(1, M):
+            # process from left to right
+            for j in range(N):
+                if j == 0:
+                    left[0] = dp[0]
                 else:
-                    left[c] = max(left[c - 1] - 1, dp[c])
-            
-            for c in range(n - 1, -1, -1):
-                if c == n-1:
-                    right[c] = dp[c]
+                    left[j] = max(dp[j], left[j-1]-1)
+            # process from right to left
+            for j in range(N-1,-1,-1):
+                if j == N-1:
+                    right[N-1] = dp[N-1]
                 else:
-                    right[c] = max(right[c + 1] - 1, dp[c])
-                    
-            for c in range(n):
-                dp[c] = points[r][c] + max(left[c], right[c])
-                
+                    right[j] = max(dp[j], right[j+1]-1)
+            # set the new max points at each column based on the max of going
+            # left to right vs right to left
+            for j in range(N):
+                dp[j] = points[i][j] + max(left[j], right[j])
         return max(dp)
