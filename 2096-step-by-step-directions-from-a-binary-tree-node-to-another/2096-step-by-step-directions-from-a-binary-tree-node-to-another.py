8d3353495028f446ef772b17e3cb34c93591173d
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-16 18:07:53 -0600

    Time: 3345 ms (26.09%), Space: 69.9 MB (72.70%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -8,6 +8,7 @@ class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
         q = deque([root])
         graph = defaultdict(list)
+        
         while q:
             node = q.popleft()
             if node.left:
@@ -20,17 +21,17 @@ class Solution:
                 graph[node.right.val].append((node.val, 'U'))
                 q.append(node.right)
         
-        
         q = deque([(startValue, '')])
         visited = set()
         visited.add(startValue)
+        
         while q:
-            node, curPath = q.popleft()
-            if node == destValue:
-                return curPath
+            node, path = q.popleft()
             
-            for neighbor, direction in graph[node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    q.append((neighbor, curPath + direction))
-        return -1
+            if node == destValue:
+                return path
+            
+            for child, direction in graph[node]:
+                if child not in visited:
+                    visited.add(child)
+                    q.append((child, path + direction))
