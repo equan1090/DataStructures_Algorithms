7927ad670ecd7b33bbd596ac182d843d42809977
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-18 19:17:27 -0600

    Time: 1600 ms (29.90%), Space: 17 MB (91.59%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,28 +1,23 @@
 class Solution:
-    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        
-        graph = defaultdict(list)
-        indegree = defaultdict(int)
-        q = deque()
-        result = []
-        
-        # everything in supplies has an indegree of 0
-        for s in supplies:
-            indegree[s] = 0
-            q.append(s)
-            
-        for i, r in enumerate(recipes):
-            for ing in ingredients[i]:
-                graph[ing].append(r)
-                indegree[r] += 1
-                
-        while q:
-            node = q.pop()
-            if node in recipes:
-                result.append(node)
-            for n in graph[node]:
-                indegree[n] -= 1
-                if indegree[n] == 0:
-                    q.append(n)
-                    
-        return result
+	def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+
+		graph = defaultdict(list)
+		in_degree = defaultdict(int)
+		for r,ing in zip(recipes,ingredients):
+			for i in ing:
+				graph[i].append(r)
+				in_degree[r]+=1
+
+		queue = supplies[::]
+		res = []
+		while queue:
+			ing = queue.pop(0)
+			if ing in recipes:
+				res.append(ing)
+
+			for child in graph[ing]:
+				in_degree[child]-=1
+				if in_degree[child]==0:
+					queue.append(child)
+
+		return res
