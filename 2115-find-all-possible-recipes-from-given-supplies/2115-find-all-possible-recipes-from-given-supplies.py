089d420310533530a6d2bba53beb8d69be252976
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-18 23:10:03 -0600

    Time: 1990 ms (13.84%), Space: 17 MB (91.59%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,26 +1,55 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        '''
+                yeast  flour
+                    \  /
+              meat  bread
+                 \  /
+               sandwich
+        '''
         
+        '''
+        graph {
+            yeast: [bread],
+            flour: [bread],
+            meat: [sandwich]
+            bread: [sandwich]
+            sandwich: []
+        }
+        num_parents = {
+            bread: 2
+            sandwich: 2
+        }
+        Input: recipes = ["bread",          "sandwich"], 
+        
+        ingredients = [["yeast","flour"],["bread","meat"]], 
+        supplies = ["yeast","flour","meat"]
+        
+        '''
         graph = defaultdict(list)
         num_parents = defaultdict(int)
         
         for r, ing in zip(recipes, ingredients):
-            
             for i in ing:
-                num_parents[r] += 1
                 graph[i].append(r)
+                num_parents[r] += 1
+        
         
         q = deque(supplies)
         res = []
-        
+        # q = [yeast, flour, meat]
         while q:
             node = q.popleft()
+            
             if node in recipes:
                 res.append(node)
-        
+            
             for child in graph[node]:
                 num_parents[child] -= 1
                 if num_parents[child] == 0:
                     q.append(child)
         return res
             
+            
+        
+        
