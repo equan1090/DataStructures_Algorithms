75919d4c6f657f3e4e34d8c5195fe168ac4cd1ae
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-21 17:35:12 -0600

    Time: 32 ms (89.08%), Space: 13.9 MB (29.34%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -9,8 +9,12 @@ class Solution:
             depth = len(p) - 1
             name = p[-1]
             l = len(name)
-            while stack and stack[-1][1] >= depth: stack.pop()
-            if not stack: stack.append((l, depth))
-            else: stack.append((l+stack[-1][0], depth))
-            if '.' in name: res = max(res, stack[-1][0] + stack[-1][1])   
+            while stack and stack[-1][1] >= depth: 
+                stack.pop()
+            if not stack: 
+                stack.append((l, depth))
+            else: 
+                stack.append((l+stack[-1][0], depth))
+            if '.' in name: 
+                res = max(res, stack[-1][0] + stack[-1][1])   
         return res
