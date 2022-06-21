74cb91340872c6c073a4987fdd0de62327ba9cd3
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-21 17:34:50 -0600

    Time: 39 ms (64.45%), Space: 13.9 MB (29.34%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -1,12 +1,16 @@
 class Solution:
-    def lengthLongestPath(self, input):
-        maxlen = 0
-        pathlen = {0: 0}
-        for line in input.splitlines():
-            name = line.lstrip('\t')
-            depth = len(line) - len(name)
-            if '.' in name:
-                maxlen = max(maxlen, pathlen[depth] + len(name))
-            else:
-                pathlen[depth + 1] = pathlen[depth] + len(name) + 1
-        return maxlen
+    def lengthLongestPath(self, s: str) -> int:
+        paths = s.split('\n')
+        stack = []
+        res = 0
+
+        for path in paths:
+            p = path.split('\t')
+            depth = len(p) - 1
+            name = p[-1]
+            l = len(name)
+            while stack and stack[-1][1] >= depth: stack.pop()
+            if not stack: stack.append((l, depth))
+            else: stack.append((l+stack[-1][0], depth))
+            if '.' in name: res = max(res, stack[-1][0] + stack[-1][1])   
+        return res
