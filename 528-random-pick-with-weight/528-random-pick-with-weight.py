23db24c88d6167f39ec36eb46dfddfed27a66331
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-21 12:15:45 -0600

    Time: 7644 ms (9.55%), Space: 18.8 MB (22.96%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -1,26 +1,22 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.prefix_sums = []
-        running_sum = 0
-        for weight in w:
-            running_sum += weight
-            self.prefix_sums.append(running_sum)
-        self.total_sum = running_sum
+        self.w = w
+		# 1. calculate relative frequency
+        denom = sum(self.w)
+        for i in range(len(self.w)):
+            self.w[i] = self.w[i] / denom
+        # 2. put relative frequencies on the number line between 0 and 1
+		# Note self.w[-1] = 1
+        for i in range(1,len(self.w)):
+            self.w[i] += self.w[i-1]
 
     def pickIndex(self) -> int:
-        target = random.random() * self.total_sum
-        left, right = 0, len(self.prefix_sums) - 1
-        while left + 1 < right:
-            mid = left + (right - left) // 2
-            if target > self.prefix_sums[mid]:
-                left = mid
-            else:
-                right = mid
-        if self.prefix_sums[left] >= target:
-            return left
-        return right
+        N = random.uniform(0, 1)
 
+        for index, weight in enumerate(self.w):
+            if N <= weight:
+                return index
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
