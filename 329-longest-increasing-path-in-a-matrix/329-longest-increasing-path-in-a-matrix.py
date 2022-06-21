a73f4a41af114adf73008d68bb807da9ae51b6bc
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-20 22:47:20 -0600

    Time: 983 ms (11.33%), Space: 16 MB (57.77%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,33 +1,31 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        longest = 0
         memo = {}
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                attempt = self.dfs(matrix, row, col, float('-inf'), memo)
-                longest = max(attempt, longest)
-        return longest
         
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                self.dfs(matrix, r, c, float('-inf'), memo)
+        return max(memo.values())
         
         
     def dfs(self, matrix, r, c, prev, memo):
         key = (r, c)
-        rowBounds = 0 <= r < len(matrix)
-        colBounds = 0 <= c < len(matrix[0])
-
-        if not rowBounds or not colBounds:
+        rowbounds = 0 <= r < len(matrix)
+        colbounds = 0 <= c < len(matrix[0])
+        
+        
+        if not rowbounds or not colbounds:
             return 0
         
         if matrix[r][c] <= prev:
             return 0
         if key in memo:
             return memo[key]
-        
         res = 1
-        res = max(res, 1 + self.dfs(matrix, r + 1, c, matrix[r][c], memo))
-        res = max(res, 1 + self.dfs(matrix, r - 1, c, matrix[r][c], memo))
-        res = max(res, 1 + self.dfs(matrix, r, c + 1, matrix[r][c], memo))
-        res = max(res, 1 + self.dfs(matrix, r, c - 1, matrix[r][c], memo))
+        res = max(res, 1 +self.dfs(matrix, r + 1, c, matrix[r][c], memo))
+        res = max(res, 1 +self.dfs(matrix, r - 1, c, matrix[r][c], memo))
+        res = max(res, 1 +self.dfs(matrix, r, c + 1, matrix[r][c], memo))
+        res = max(res, 1 +self.dfs(matrix, r, c - 1, matrix[r][c], memo))
         
         memo[key] = res
         return memo[key]
