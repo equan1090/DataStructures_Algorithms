1236f5858c77c1b41ded66749c107e8889ea3a3c
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-22 17:50:11 -0600

    Time: 1256 ms (5.68%), Space: 19.5 MB (16.07%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,26 +1,27 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        res = 0
+        
+        longest = 0
         memo = {}
         for r in range(len(matrix)):
             for c in range(len(matrix[0])):
                 attempt = self.dfs(matrix, r, c, float('-inf'), memo)
-                res = max(res, attempt)
-        return res
-            
+                longest = max(attempt, longest)
+        return longest
+        
+        
     def dfs(self, matrix, r, c, prev, memo):
+        key = (r, c, prev)
+        if key in memo:
+            return memo[key]
+        
         rowbounds = 0 <= r < len(matrix)
         colbounds = 0 <= c < len(matrix[0])
         
         if not rowbounds or not colbounds or matrix[r][c] <= prev:
             return 0
         
-        key = (r, c)
-        if key in memo:
-            return memo[key]
-        
         res = 1
-        
         res = max(res, 1 + self.dfs(matrix, r + 1, c, matrix[r][c], memo))
         res = max(res, 1 + self.dfs(matrix, r - 1, c, matrix[r][c], memo))
         res = max(res, 1 + self.dfs(matrix, r, c + 1, matrix[r][c], memo))
