9f835e2ffb66eb20de821730c0add8e0e1a3fbe7
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-21 18:16:16 -0600

    Time: 26 ms (97.90%), Space: 13.8 MB (97.82%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -1,17 +1,20 @@
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
-        max_so_far = 0
         paths = {}
+        res = 0
         chunks = input.split('\n')
+        
         for c in chunks:
             level = 0
+            
             while level < len(c) and c[level] == '\t':
                 level += 1
             
-            path_len = len(c) if level == 0 else paths[level-1] + 1 + len(c[level:])
+            length = len(c) if level == 0 else paths[level - 1] + 1 + len(c[level:])
             
             if '.' in c:
-                max_so_far = max(max_so_far, path_len)
+                res = max(res, length)
             else:
-                paths[level] = path_len
-        return max_so_far
+                paths[level] = length
+        return res
+        
