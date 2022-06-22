827fea62b2206f3823e9e4aad4b2cff91acd6d8b
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-21 18:04:50 -0600

    Time: 50 ms (31.62%), Space: 13.8 MB (73.62%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -1,20 +1,17 @@
 class Solution:
-    def lengthLongestPath(self, s: str) -> int:
-        paths = s.split('\n')
-        stack = []
-        res = 0
-
-        for path in paths:
-            p = path.split('\t')
-            depth = len(p) - 1
-            name = p[-1]
-            l = len(name)
-            while stack and stack[-1][1] >= depth: 
-                stack.pop()
-            if not stack: 
-                stack.append((l, depth))
-            else: 
-                stack.append((l+stack[-1][0], depth))
-            if '.' in name: 
-                res = max(res, stack[-1][0] + stack[-1][1])   
-        return res
+    def lengthLongestPath(self, input: str) -> int:
+        max_so_far = 0
+        paths = {}
+        chunks = input.split('\n')
+        for c in chunks:
+            level = 0
+            while level < len(c) and c[level] == '\t':
+                level += 1
+            
+            path_len = len(c) if level == 0 else paths[level-1] + 1 + len(c[level:])
+            
+            if '.' in c:
+                max_so_far = max(max_so_far, path_len)
+            else:
+                paths[level] = path_len
+        return max_so_far
