74ee587a0d1503a0e884d0854521b515b362be86
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-22 18:10:41 -0600

    Time: 78 ms (5.64%), Space: 14 MB (29.27%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.py
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.py
@@ -1,20 +1,31 @@
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
-        paths = {}
+        '''
+        "dir
+        \tsubdir1
+        \t\tfile1.ext
+        \t\tsubsubdir1
+        \tsubdir2
+        \t\tsubsubdir2
+        \t\t\tfile2.ext"
+        '''
+        dic = {}
         res = 0
-        
         chunks = input.split('\n')
         
         for c in chunks:
             level = 0
             
-            while level < len(c) and c[level] == '\t':
+            while c[level] == '\t':
                 level += 1
             
-            curLength = len(c) if level == 0 else paths[level - 1] + 1 + len(c[level:])
+            curLen = len(c) if level == 0 else dic[level - 1] + 1 + len(c[level:])
             
             if '.' in c:
-                res = max(res, curLength)
+                res = max(res, curLen)
             else:
-                paths[level] = curLength
+                dic[level] = curLen
         return res
+            
+        
+        
