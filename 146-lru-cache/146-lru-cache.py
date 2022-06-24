12d24f5994b9a6e4c656fe1e726efd4314ffbbdb
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-24 13:41:11 -0600

    Time: 1178 ms (53.39%), Space: 74.8 MB (89.41%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,55 +1,19 @@
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-
 class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.dic = dict() # key to node
-        self.capacity = capacity
-        self.head = ListNode(0, 0)
-        self.tail = ListNode(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+    def __init__(self, capacity):
+        self.cache = collections.OrderedDict()
+        self.cap = capacity
 
     def get(self, key: int) -> int:
-        if key in self.dic:
-            node = self.dic[key]
-            self.removeFromList(node)
-            self.insertIntoHead(node)
-            return node.value
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
         else:
-            return -1
+            return -1 
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dic:             # similar to get()        
-            node = self.dic[key]
-            self.removeFromList(node)
-            self.insertIntoHead(node)
-            node.value = value         # replace the value len(dic)
-        else: 
-            if len(self.dic) >= self.capacity:
-                self.removeFromTail()
-            node = ListNode(key,value)
-            self.dic[key] = node
-            self.insertIntoHead(node)
-			
-    def removeFromList(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-    
-    def insertIntoHead(self, node):
-        headNext = self.head.next 
-        self.head.next = node 
-        node.prev = self.head 
-        node.next = headNext 
-        headNext.prev = node
-    
-    def removeFromTail(self):
-        if len(self.dic) == 0: return
-        tail_node = self.tail.prev
-        del self.dic[tail_node.key]
-        self.removeFromList(tail_node)
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) == self.cap:
+            self.cache.popitem(last=False)
+
+        self.cache[key] = value  
