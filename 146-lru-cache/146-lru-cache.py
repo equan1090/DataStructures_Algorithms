b86aa32714852ca33332777e511e1d7e9df0d43d
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-24 16:27:44 -0600

    Time: 1231 ms (47.83%), Space: 76.6 MB (9.94%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,19 +1,53 @@
-class LRUCache:
-    def __init__(self, capacity):
-        self.cache = collections.OrderedDict()
-        self.cap = capacity
+class DLL:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+        
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        
+        self.left = DLL(0, 0)
+        self.right = DLL(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        
+        prev.next, nxt.prev = nxt, prev
+        
+    
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+        
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+    
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        else:
-            return -1 
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        
+        
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.pop(key)
-        elif len(self.cache) == self.cap:
-            self.cache.popitem(last=False)
-
-        self.cache[key] = value  
+            self.remove(self.cache[key])
+        self.cache[key] = DLL(key, value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.cap:
+            
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
