1b9d79d9036ef956328f2cc2d2ab75d6c8b0d484
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-26 13:42:48 -0600

    Time: 135 ms (56.41%), Space: 13.8 MB (81.60%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(list)
-        cols = defaultdict(list)
-        squares = defaultdict(list)
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
         
         for r in range(len(board)):
             for c in range(len(board[0])):
@@ -13,7 +13,7 @@ class Solution:
                 if pos in rows[r] or pos in cols[c] or pos in squares[(r // 3, c // 3)]:
                     return False
                 
-                rows[r].append(pos)
-                cols[c].append(pos)
-                squares[(r//3,c//3)].append(pos)
+                rows[r].add(pos)
+                cols[c].add(pos)
+                squares[(r//3,c//3)].add(pos)
         return True
