566809eb342f353d68c001d3670a985021546eb7
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-26 13:35:40 -0600

    Time: 383 ms (32.60%), Space: 21.1 MB (80.74%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,5 +1,11 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        product = [1, 1, 1, 1]
+        left = [1, 1, 2, 6]
+        Input: nums = [1,2,3,4]
+        Output: [24,12,8,6]
+        '''
         product = [1] * len(nums)
         
         left = 1
@@ -8,7 +14,7 @@ class Solution:
             left *= nums[i]
         
         right = 1
-        for i in range(len(nums) -1, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             product[i] *= right
             right *= nums[i]
         return product
