1aba5a140632e90db288ebf1800529759d5dd52f
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-06-29 17:08:06 -0600

    Time: 1373 ms (36.18%), Space: 76.6 MB (10.12%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,55 +1,58 @@
-class ListNode:
-    def __init__(self, key, value):
+class DLL:
+    def __init__(self, key, val):
         self.key = key
-        self.value = value
+        self.val = val
         self.prev = None
         self.next = None
+        
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.dic = dict() # key to node
-        self.capacity = capacity
-        self.head = ListNode(0, 0)
-        self.tail = ListNode(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.cap = capacity
+        self.cache = {}
+        
+        self.left = DLL(0, 0)
+        self.right = DLL(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
 
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        
+        prev.next, nxt.prev = nxt, prev
+        
+    
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+        
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+    
     def get(self, key: int) -> int:
-        if key in self.dic:
-            node = self.dic[key]
-            self.removeFromList(node)
-            self.insertIntoHead(node)
-            return node.value
-        else:
-            return -1
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        
+        
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dic:             # similar to get()        
-            node = self.dic[key]
-            self.removeFromList(node)
-            self.insertIntoHead(node)
-            node.value = value         # replace the value len(dic)
-        else: 
-            if len(self.dic) >= self.capacity:
-                self.removeFromTail()
-            node = ListNode(key,value)
-            self.dic[key] = node
-            self.insertIntoHead(node)
-			
-    def removeFromList(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-    
-    def insertIntoHead(self, node):
-        headNext = self.head.next 
-        self.head.next = node 
-        node.prev = self.head 
-        node.next = headNext 
-        headNext.prev = node
-    
-    def removeFromTail(self):
-        if len(self.dic) == 0: return
-        tail_node = self.tail.prev
-        del self.dic[tail_node.key]
-        self.removeFromList(tail_node)
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = DLL(key, value)
+        self.insert(self.cache[key])
+        
+        if len(self.cache) > self.cap:
+            
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
