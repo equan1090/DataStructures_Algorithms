fb9c6fff88af9d3331ac0a37ca948edd73247961
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-07-01 16:50:03 -0600

    Time: 51 ms (57.09%), Space: 14.1 MB (76.70%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,18 +1,19 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        self.dfs(n, n, [], res)
-        return res
-    
+        
+        return self.dfs(n, n, '', [])
+        
     def dfs(self, opened, closed, path, res):
-        # if we ever have more opened than closes remaining, then that means we added a closed before an open parentheses
-        # example we start with 3 opened, 3 closed, if we end up inputting '())', that means we have 2 opened left with 1 closed left, 3 - 1 vs 3 - 2
+        
         if opened > closed or opened < 0 or closed < 0:
             return
         
         if opened == 0 and closed == 0:
             res.append(''.join(path))
+            return
         
-        self.dfs(opened - 1, closed, path + ['('], res)
-        self.dfs(opened, closed - 1, path + [')'], res)
+        self.dfs(opened - 1, closed, path + '(', res)
+        self.dfs(opened, closed - 1, path + ')', res)
+        
         return res
+        
