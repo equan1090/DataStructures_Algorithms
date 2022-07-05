7b1acb173207a60dbf63461d6d7377e9d137eff3
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-07-04 22:29:45 -0600

    Time: 9789 ms (5.02%), Space: 13.9 MB (50.78%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,12 +1,14 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        
         visited = set()
         for r in range(len(board)):
             for c in range(len(board[0])):
                 if self.dfs(board, r, c, 0, word, visited):
                     return True
         return False
-    
+        
+        
     def dfs(self, board, r, c, i, word, visited):
         rowbounds = 0 <= r < len(board)
         colbounds = 0 <= c < len(board[0])
@@ -14,24 +16,20 @@ class Solution:
         if i == len(word):
             return True
         
-
-        if not rowbounds or not colbounds:
-            return False
-        
-        if word[i] != board[r][c]:
+        if not rowbounds or not colbounds or board[r][c] != word[i]:
             return False
         
         pos = (r, c)
         if pos in visited:
             return False
-        
         visited.add(pos)
         
         res = (
-            self.dfs(board, r + 1, c, i + 1, word, visited) or
-            self.dfs(board, r - 1, c, i + 1, word, visited) or
-            self.dfs(board, r, c + 1, i + 1, word, visited) or
+            self.dfs(board, r + 1, c, i + 1, word, visited) or 
+            self.dfs(board, r - 1, c, i + 1, word, visited) or 
+            self.dfs(board, r, c + 1, i + 1, word, visited) or 
             self.dfs(board, r, c - 1, i + 1, word, visited)
         )
         visited.remove(pos)
         return res
+        
