a4e5902c7adadaf0c1a5213f6776f921d16b2cd1
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-07-07 17:17:32 -0600

    Time: 46 ms (60.65%), Space: 13.9 MB (69.72%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         values = []
