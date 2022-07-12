27ca33adcbfde32d4f76c6a1ba0cb69aa59fe2e0
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-07-11 18:57:34 -0600

    Time: 177 ms (91.83%), Space: 15 MB (73.94%) - LeetHub

---
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        flips = 0
+        potential = 0
+        for c in s:
+            if c == '1':
+                potential += 1
+            elif potential > 0:
+                flips += 1
+                potential -= 1
+        
+        
+        return flips
