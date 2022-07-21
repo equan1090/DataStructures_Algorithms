dbd8615690df4dfdb6e57b27824c5ae8696b3021
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-07-21 15:53:55 -0600

    Time: 1545 ms (27.14%), Space: 76.7 MB (10.75%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -4,7 +4,7 @@ class DoubleLL:
         self.val = val
         self.next = None
         self.prev = None
-
+    
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -13,24 +13,26 @@ class LRUCache:
         self.left = DoubleLL(-1, -1)
         self.right = DoubleLL(-1, -1)
         self.left.next, self.right.prev = self.right, self.left
-        
 
     def remove(self, node):
-        prev = node.prev
-        nxt = node.next
+        prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
     
     def insert(self, node):
         prev = self.right.prev
         prev.next, node.prev = node, prev
         node.next, self.right.prev = self.right, node
-    
+        
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
+            
             return self.cache[key].val
+        
         return -1
+        
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
