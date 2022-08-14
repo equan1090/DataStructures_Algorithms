e0a1853ab7e11236853b3fe27c66cb418f8757b9
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-08-13 22:48:18 -0600

    Time: 1401 ms (42.75%), Space: 76.6 MB (24.62%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -4,34 +4,24 @@ class DoubleLL:
         self.val = val
         self.next = None
         self.prev = None
-    
+        
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
         self.left = DoubleLL(-1, -1)
         self.right = DoubleLL(-1, -1)
         self.left.next, self.right.prev = self.right, self.left
+        self.cache = {}
 
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-    
-    def insert(self, node):
-        prev = self.right.prev
-        prev.next, node.prev = node, prev
-        node.next, self.right.prev = self.right, node
-        
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
-            
             return self.cache[key].val
         
         return -1
-        
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
@@ -44,9 +34,21 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+            
+        
+    
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+        
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next, node.prev = node, prev
+        node.next, self.right.prev = self.right, node
         
         
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
