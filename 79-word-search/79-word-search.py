9c837e80d1ea7fc553f9aec0b502201515acc908
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-08-15 13:22:54 -0600

    Time: 6588 ms (67.42%), Space: 13.9 MB (92.81%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,34 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
         visited = set()
         for r in range(len(board)):
             for c in range(len(board[0])):
-                if self.dfs(board, r, c, 0, word, visited):
+                if self.dfs(board, r, c, word, 0, visited):
                     return True
         return False
-        
-        
-    def dfs(self, board, r, c, i, word, visited):
+    
+    def dfs(self, board, r, c, word, i, visited):
         rowbounds = 0 <= r < len(board)
         colbounds = 0 <= c < len(board[0])
         
         if i == len(word):
             return True
         
-        if not rowbounds or not colbounds or board[r][c] != word[i]:
+        if not rowbounds or not colbounds or word[i] != board[r][c] or (r, c) in visited:
             return False
         
-        pos = (r, c)
-        if pos in visited:
-            return False
-        visited.add(pos)
+        visited.add((r, c))
         
         res = (
-            self.dfs(board, r + 1, c, i + 1, word, visited) or 
-            self.dfs(board, r - 1, c, i + 1, word, visited) or 
-            self.dfs(board, r, c + 1, i + 1, word, visited) or 
-            self.dfs(board, r, c - 1, i + 1, word, visited)
+                self.dfs(board, r + 1, c, word, i+1, visited) or
+            self.dfs(board, r - 1, c, word, i+1, visited) or
+            self.dfs(board, r, c + 1, word, i+1, visited) or
+            self.dfs(board, r, c - 1, word, i+1, visited)
+        
         )
-        visited.remove(pos)
+
+        
+        visited.remove((r, c))
         return res
