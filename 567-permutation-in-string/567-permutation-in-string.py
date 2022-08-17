62c9b6a1e6ae705893a0de78de7012c412405e4f
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-08-17 13:27:18 -0600

    Time: 131 ms (51.79%), Space: 13.9 MB (69.26%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,16 +1,15 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
         if len(s1) > len(s2):
             return False
         
-        d1 = {i:0 for i in range(26)}
-        d2 = {i:0 for i in range(26)}
+        d1 = {x:0 for x in range(26)}
+        d2 = {x: 0 for x in range(26)}
         
         for i in range(len(s1)):
             d1[ord(s1[i]) - ord('a')] += 1
             d2[ord(s2[i]) - ord('a')] += 1
-            
+        
         start = 0
         end = len(s1) - 1
         
@@ -22,7 +21,6 @@ class Solution:
             start += 1
             end += 1
             if end < len(s2):
-                
                 d2[ord(s2[end]) - ord('a')] += 1
         return False
-            
+        
