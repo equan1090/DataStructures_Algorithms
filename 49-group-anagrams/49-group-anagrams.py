e17646af0b3974641082a4928bf29e0758ea50ca
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-08-21 20:40:37 -0600

    Time: 181 ms (36.58%), Space: 19.9 MB (20.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,15 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = {}
-        
+        freq = {}
         for s in strs:
             count = [0] * 26
             for c in s:
                 count[ord(c) - ord('a')] += 1
             
             key = tuple(count)
-            if key not in group:
-                group[key] = []
-            group[key].append(s)
-        
-        return group.values()
+            if key not in freq:
+                freq[key] = []
+            freq[key].append(s)
+        return freq.values()
