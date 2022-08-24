d6c971610f80fde4d952f853adf585b5f93a73d7
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-08-24 12:52:56 -0600

    Time: 2734 ms (54.94%), Space: 30.2 MB (29.79%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -9,12 +9,12 @@ class Solution:
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
             q.append(r)
-            
-            if l > q[0]:
-                q.popleft()
-            
             if (r+1) >= k:
                 res.append(nums[q[0]])
                 l+=1
+            if l > q[0]:
+                q.popleft()
+            
+
             r+=1
         return res
