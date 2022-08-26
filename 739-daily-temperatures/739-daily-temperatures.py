b7e969f7ee88b2db666883a2dbdb4d04ec4d8c60
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-08-26 00:25:27 -0600

    Time: 1569 ms (71.14%), Space: 24.6 MB (91.51%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -2,11 +2,11 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
         stack = []
-        
-        for idx, temps in enumerate(temperatures):
-            while stack and stack[-1][-1] < temps:
-                i, t = stack.pop()
-                res[i] = idx - i
-                
-            stack.append([idx, temps])
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][1] < t:
+                idx, temp = stack.pop()
+                res[idx] = i - idx
+            
+            stack.append([i, t])
         return res
+        
