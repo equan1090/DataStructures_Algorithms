fc0efdbf19943cccbba212d5e57096c1f492ff4f
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-01 16:43:26 -0600

    Time: 72 ms (68.54%), Space: 18.5 MB (98.13%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -6,14 +6,14 @@
 #         self.right = right
 class Solution(object):
     def isBalanced(self, root):
+        
             
-        def check(root):
-            if root is None:
-                return 0
-            left  = check(root.left)
-            right = check(root.right)
-            if left == -1 or right == -1 or abs(left - right) > 1:
-                return -1
-            return 1 + max(left, right)
-            
-        return check(root) != -1
+        return self.check(root) != -1
+    def check(self, root):
+        if root is None:
+            return 0
+        left  = self.check(root.left)
+        right = self.check(root.right)
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        return 1 + max(left, right)
