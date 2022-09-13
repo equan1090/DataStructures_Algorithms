4c2ac712378f546c92833c2ed1840881411de148
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-13 15:44:40 -0600

    Time: 732 ms (6.93%), Space: 14.7 MB (26.36%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -5,11 +5,19 @@ class Solution:
         for char in s:
             memo[char] += 1
 			
-        count = 0
+
         for char in t:
             if memo[char]:
                 memo[char] -=1   # if char in t is also in memo, substract that from the counted number
-            else:
-                count += 1
+
         # return count #or
         return sum(memo.values())
+    
+    '''
+    memo = {l: 1
+            e: 3
+            t: 1
+            c: 1
+            o: 1
+            d: 1}
+    '''
