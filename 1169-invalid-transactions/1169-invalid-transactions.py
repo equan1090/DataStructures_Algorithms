1f62b986476788eb71b26768354f68180c3701cf
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-13 12:42:48 -0600

    Time: 123 ms (56.40%), Space: 14.8 MB (40.16%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,22 +1,60 @@
-class Solution:
-    def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        if not transactions:
-            return []
-        
-        memo = collections.defaultdict(list)
-        invalid_tran = set()
-        for i, transaction in enumerate(transactions):
-            name, time, amount, city = (int(i) if i.isnumeric() else i for i in transaction.split(','))
-            
-            if amount > 1000:
-                invalid_tran.add((transaction, i))               
-                 
-            if name in memo:
-                for tran, idx in memo[name]:
-                    _, prev_time, _, prev_city = (int(i) if i.isnumeric() else i for i in  tran.split(','))
-                    if abs(time-prev_time) <= 60 and prev_city != city:
-                        invalid_tran.add((tran, idx))
-                        invalid_tran.add((transaction, i))                    
-            
-            memo[name].append((transaction, i))
-        return [ele[0] for ele in invalid_tran]
+from collections import defaultdict
+
+""" 
+https://www.notion.so/paulonteri/Hashtables-Hashsets-220d9f0e409044c58ec6c2b0e7fe0ab5#cf22995975274881a28b544b0fce4716
+"""
+
+class Solution(object):
+    def invalidTransactions(self, transactions):
+        """ 
+        - Record all transactions done at a particular time. Recording the person and the location. Example:
+            `['alice,20,800,mtv','bob,50,1200,mtv','bob,20,100,beijing']` :\n
+            ` 
+            {   
+            20: {'alice': {'mtv'}, 'bob': {'beijing'}}, 
+            50: {'bob': {'mtv'}}
+            } 
+            ` \n
+            `{time: {person: {location}, person2: {location1, location2}}, time: {person: {location}}}`
+        - For each transaction, check if the amount is invalid - and add it to the invalid transactions if so.
+        - For each transaction, go through invalid times (+-60), check if a transaction by the same person happened
+            in a different city - and add it to the invalid transactions if so.
+        """
+        invalid = []
+
+        # Record all transactions done at a particular time
+        #   including the person and the location.
+        transaction_time = defaultdict(dict)
+        for transaction in transactions:
+            name, str_time, amount, city = transaction.split(",")
+            time = int(str_time)
+
+            if name not in transaction_time[time]:
+                transaction_time[time][name] = {city, }
+            else:
+                transaction_time[time][name].add(city)
+
+        for transaction in transactions:
+            name, str_time, amount, city = transaction.split(",")
+            time = int(str_time)
+
+            # # check amount
+            if int(amount) > 1000:
+                invalid.append(transaction)
+                continue
+
+            # # check if person did transaction within 60 minutes in a different city
+            for inv_time in range(time-60, time+61):
+                if inv_time not in transaction_time:
+                    continue
+                if name not in transaction_time[inv_time]:
+                    continue
+
+                trans_by_name_at_time = transaction_time[inv_time][name]
+
+                # check if transactions were done in a different city
+                if city not in trans_by_name_at_time or len(trans_by_name_at_time) > 1:
+                    invalid.append(transaction)
+                    break
+
+        return invalid
