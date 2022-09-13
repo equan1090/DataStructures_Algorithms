d5c1ba42042b2dbba95218dd4e5b78b7bc8ea879
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-13 00:52:38 -0600

    Time: 81 ms (87.51%), Space: 14.3 MB (38.09%) - LeetHub

--- 1244-design-a-leaderboard/1244-design-a-leaderboard.py
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.py
@@ -1,29 +1,23 @@
 class Leaderboard:
 
     def __init__(self):
-        self.player = {}
-        
+        self.cache = {}
 
     def addScore(self, playerId: int, score: int) -> None:
-        if playerId not in self.player:
-            self.player[playerId] = 0
-        self.player[playerId] += score
+        if playerId not in self.cache:
+            self.cache[playerId] = 0
+        self.cache[playerId] += score
 
     def top(self, K: int) -> int:
         heap = []
-        
-        for val in self.player.values():
+        for val in self.cache.values():
             heapq.heappush(heap, val)
-            
             if len(heap) > K:
                 heapq.heappop(heap)
-               
-        
         return sum(heap)
-            
 
     def reset(self, playerId: int) -> None:
-        del self.player[playerId]
+        del self.cache[playerId]
 
 
 # Your Leaderboard object will be instantiated and called as such:
