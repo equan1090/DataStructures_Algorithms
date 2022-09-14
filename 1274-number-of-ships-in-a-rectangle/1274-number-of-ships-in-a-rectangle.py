1a9755568802c90b38a12aeeac01dac25b355d62
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-13 18:12:32 -0600

    Time: 98 ms (6.37%), Space: 13.9 MB (68.01%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.py
@@ -12,45 +12,42 @@
 
 class Solution:
     def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
-
-        return self.findShip(sea, topRight, bottomLeft)
         
-        
-    def findShip(self, sea, topRight, bottomLeft):
+        return self.findShips(sea, topRight, bottomLeft)
+    
+    def findShips(self, sea, topRight, bottomLeft):
         if topRight.x < bottomLeft.x or topRight.y < bottomLeft.y:
             return 0
         
         if topRight.x == bottomLeft.x and topRight.y == bottomLeft.y:
             if sea.hasShips(topRight, bottomLeft):
                 return 1
-            else:
-                return 0
-        
+            return 0
         
         if not sea.hasShips(topRight, bottomLeft):
             return 0
         
-        midX = (topRight.x + bottomLeft.x) // 2
-        midY = (topRight.y + bottomLeft.y) // 2
+        midX = (bottomLeft.x + topRight.x) // 2
+        midY = (bottomLeft.y + topRight.y) // 2
         mid = Point(midX, midY)
         
-        topLeftQ = self.findShip(sea, Point(mid.x, topRight.y), Point(bottomLeft.x, mid.y+1))
-        topRightQ = self.findShip(sea, topRight, Point(mid.x+1, mid.y+1))
-        bottomLeftQ = self.findShip(sea, Point(mid.x, mid.y), bottomLeft)
-        bottomRightQ = self.findShip(sea, Point(topRight.x, mid.y), Point(mid.x+1, bottomLeft.y))
+        topLeftQ = self.findShips(sea, Point(mid.x, topRight.y), Point(bottomLeft.x, mid.y+1))
+        topRightQ = self.findShips(sea, topRight, Point(mid.x+1, mid.y+1))
+        bottomLeftQ = self.findShips(sea, Point(mid.x, mid.y), bottomLeft)
+        bottomRightQ = self.findShips(sea, Point(topRight.x, mid.y), Point(mid.x+1, bottomLeft.y))
         return topLeftQ + topRightQ + bottomLeftQ + bottomRightQ
         
-        '''
+        
+        '''       
         -----------------
         |       |       |
-        |       |       |
-mid+1   |---------------
-        |       |       |
+m  +1   |       |my+1   |
+        |---------------
+mid     |       |       |
         |       |       |
 BLx     |       |       |
         -----------------
                mx mx+1
         '''
-        
         
         
