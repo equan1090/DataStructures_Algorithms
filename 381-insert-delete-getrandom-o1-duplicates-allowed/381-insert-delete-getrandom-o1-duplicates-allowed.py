96b1812d64861f5e0b1d991e119ac4bc427bad81
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-16 20:07:53 -0600

    Time: 1022 ms (24.41%), Space: 65.9 MB (51.12%) - LeetHub

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -0,0 +1,32 @@
+class RandomizedCollection:
+
+    def __init__(self):
+        self.hashmap = defaultdict(set)
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        self.hashmap[val].add(len(self.list))
+        self.list.append(val)
+        return len(self.hashmap[val]) == 1
+
+    def remove(self, val: int) -> bool:
+        if not self.hashmap[val]:
+            return False
+        last = self.list[-1]
+        idx = self.hashmap[val].pop()
+        self.list[idx] = last
+        self.hashmap[last].add(idx)
+        
+        self.hashmap[last].discard(len(self.list) - 1)
+        self.list.pop()
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.list)
+
+
+# Your RandomizedCollection object will be instantiated and called as such:
+# obj = RandomizedCollection()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
