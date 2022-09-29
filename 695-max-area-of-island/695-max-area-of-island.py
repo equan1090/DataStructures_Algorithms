bdf8acc11b18d5823e6e60353adfb63a278b9af8
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-29 15:41:44 -0600

    Time: 388 ms (12.78%), Space: 17.8 MB (18.67%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -5,30 +5,22 @@ class Solution:
         
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                attempt = self.traverse(grid, r, c, visited)
-                res = max(res, attempt)
+                potential = self.findIsland(grid, r, c, visited)
+                res = max(res, potential)
         return res
     
-    
-    def traverse(self, grid, r, c, visited):
+    def findIsland(self, grid, r, c, visited):
         rowbounds = 0 <= r < len(grid)
         colbounds = 0 <= c < len(grid[0])
-        
-        if not rowbounds or not colbounds:
+        if not rowbounds or not colbounds or (r, c) in visited or grid[r][c] == 0:
             return 0
         
-        if grid[r][c] == 0:
-            return 0
-        
-        pos = (r, c)
-        if pos in visited:
-            return 0
-        visited.add(pos)
+        visited.add((r, c))
         
         size = 1
-        size += self.traverse(grid, r + 1, c, visited)
-        size += self.traverse(grid, r - 1, c, visited)
-        size += self.traverse(grid, r, c + 1, visited)
-        size += self.traverse(grid, r, c - 1, visited)
+        
+        size += self.findIsland(grid, r + 1, c, visited)
+        size += self.findIsland(grid, r - 1, c, visited)
+        size += self.findIsland(grid, r, c + 1, visited)
+        size += self.findIsland(grid, r, c - 1, visited)
         return size
-    
