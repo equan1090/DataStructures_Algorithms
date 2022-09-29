c8396cbc395739433dcda54a2ab2b0ed05d35303
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-29 16:15:28 -0600

    Time: 292 ms (92.53%), Space: 15.6 MB (54.77%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,31 +1,34 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacific = set()
+        atlantic = set()
         ROWS = len(heights)
         COLS = len(heights[0])
-        pac = set()
-        atl = set()
-        
-        def dfs(r, c, visit, prevHeight):
-            if ((r,c) in visit or r < 0 or c < 0 or r == ROWS or c == COLS or heights[r][c] < prevHeight):
-                return
-            
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
         
         for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+            self.dfs(heights, 0, c, pacific, heights[0][c])
+            self.dfs(heights, ROWS - 1, c, atlantic, heights[ROWS-1][c])
             
         for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
-        
+            self.dfs(heights, r, 0, pacific, heights[r][0])
+            self.dfs(heights, r, COLS - 1, atlantic, heights[r][COLS-1])
+            
         res = []
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
+                if (r, c) in atlantic and (r, c) in pacific:
                     res.append([r, c])
         return res
+            
+    def dfs(self, heights, r, c, visited, prev):
+        rowbounds = 0 <= r < len(heights)
+        colbounds = 0 <= c < len(heights[0])
+        if ((r, c) in visited or not rowbounds or not colbounds or heights[r][c] < prev):
+            return
+        
+        visited.add((r, c))
+        self.dfs(heights, r + 1, c, visited, heights[r][c])
+        self.dfs(heights, r - 1, c, visited, heights[r][c])
+        self.dfs(heights, r, c + 1, visited, heights[r][c])
+        self.dfs(heights, r, c - 1, visited, heights[r][c])
