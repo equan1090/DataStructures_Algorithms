60213ca2d48576f318ff59a8242a17358d148277
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-29 16:53:51 -0600

    Time: 780 ms (20.01%), Space: 15.5 MB (77.91%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -4,30 +4,31 @@ class Solution:
         atlantic = set()
         ROWS = len(heights)
         COLS = len(heights[0])
-        
+        for r in range(ROWS):
+            self.dfs(heights, r, 0, pacific, heights[r][0])
+            self.dfs(heights, r, COLS - 1, atlantic, heights[r][COLS - 1])
+            
         for c in range(COLS):
             self.dfs(heights, 0, c, pacific, heights[0][c])
             self.dfs(heights, ROWS - 1, c, atlantic, heights[ROWS-1][c])
-            
-        for r in range(ROWS):
-            self.dfs(heights, r, 0, pacific, heights[r][0])
-            self.dfs(heights, r, COLS - 1, atlantic, heights[r][COLS-1])
-            
-        res = []
         
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in atlantic and (r, c) in pacific:
+                if (r, c) in pacific and (r, c) in atlantic:
                     res.append([r, c])
+                    
         return res
-            
+    
     def dfs(self, heights, r, c, visited, prev):
         rowbounds = 0 <= r < len(heights)
         colbounds = 0 <= c < len(heights[0])
-        if ((r, c) in visited or not rowbounds or not colbounds or heights[r][c] < prev):
+        
+        if not rowbounds or not colbounds or (r, c) in visited or heights[r][c] < prev:
             return
         
         visited.add((r, c))
+        
         self.dfs(heights, r + 1, c, visited, heights[r][c])
         self.dfs(heights, r - 1, c, visited, heights[r][c])
         self.dfs(heights, r, c + 1, visited, heights[r][c])
