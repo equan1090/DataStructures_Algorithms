8c6f71fd194a70726a57113593cde14ef0e0f01e
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-09-29 14:51:33 -0600

    Time: 1146 ms (5.02%), Space: 21.7 MB (28.92%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -2,24 +2,25 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
         visited = set()
+        
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if self.dfs(grid, r, c, visited):
+                if self.findIsland(grid, r, c, visited):
                     count += 1
         return count
     
-    def dfs(self, grid, r, c, visited):
+    def findIsland(self, grid, r, c, visited):
         rowbounds = 0 <= r < len(grid)
         colbounds = 0 <= c < len(grid[0])
         
-        if not rowbounds or not colbounds or (r, c) in visited or grid[r][c] == '0':
+        if not rowbounds or not colbounds or grid[r][c] == '0' or (r, c) in visited:
             return False
         
         visited.add((r, c))
         
-        self.dfs(grid, r + 1, c, visited)
-        self.dfs(grid, r - 1, c, visited)
-        self.dfs(grid, r, c + 1, visited)
-        self.dfs(grid, r, c - 1, visited)
+        self.findIsland(grid, r + 1, c, visited)
+        self.findIsland(grid, r - 1, c, visited)
+        self.findIsland(grid, r, c + 1, visited)
+        self.findIsland(grid, r, c - 1, visited)
         
         return True
