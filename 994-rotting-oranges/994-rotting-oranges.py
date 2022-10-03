8e16bc550022dca8c289a6eee9bae52bc12f2734
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-02 21:55:59 -0600

    Time: 54 ms (93.57%), Space: 13.9 MB (48.02%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -3,30 +3,31 @@ class Solution:
         q = deque([])
         res = 0
         fresh = 0
+        
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 2:
                     q.append((r, c))
                 if grid[r][c] == 1:
                     fresh += 1
-                    
-        directions = [[1, 0], [-1, 0], [0, -1], [0, 1]]
+        
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         while q and fresh > 0:
             
             for i in range(len(q)):
                 r, c = q.popleft()
                 
+                
                 for dr, dc in directions:
                     nRow = r + dr
                     nCol = c + dc
+                    
                     rowbounds = 0 <= nRow < len(grid)
                     colbounds = 0 <= nCol < len(grid[0])
                     
                     if rowbounds and colbounds and grid[nRow][nCol] == 1:
-                        fresh -= 1
-                        grid[nRow][nCol] = 2
                         q.append((nRow, nCol))
+                        grid[nRow][nCol] = 2
+                        fresh -= 1
             res += 1
-        
         return res if fresh == 0 else -1
-                
