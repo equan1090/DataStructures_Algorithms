be68f6e3fcfcccd12394c1d7aa6ed13c75a3257b
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-06 12:07:25 -0600

    Time: 774 ms (59.42%), Space: 14.2 MB (90.41%) - LeetHub

--- 839-similar-string-groups/839-similar-string-groups.py
+++ 839-similar-string-groups/839-similar-string-groups.py
@@ -1,37 +1,34 @@
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
-        #Create adjacency List
         graph = {}
-        for word in strs:
-            graph[word]=[]
-
+        for s in strs:
+            graph[s] = []
         for i in range(len(strs)):
-            for j in range(i+1,len(strs)):
-                differences=0
+            for j in range(i + 1, len(strs)):
+                differences = 0
                 for k in range(len(strs[0])):
                     if strs[i][k] != strs[j][k]:
-                        differences+=1
-                        if differences>2:
+                        differences += 1
+                        
+                        if differences > 2:
                             break
-                if differences==2:
+                    
+                if differences == 2:
                     graph[strs[i]].append(strs[j])
                     graph[strs[j]].append(strs[i])
-                    
+        
+        
         visited = set()
         count = 0
         for node in graph:
-            if self.dfs(graph, node, visited):
+            if self.traverse(graph, node, visited):
                 count += 1
         return count
     
-    
-    def dfs(self, graph, node, visited):
+    def traverse(self, graph, node, visited):
         if node in visited:
             return False
         visited.add(node)
-        
         for neighbor in graph[node]:
-            self.dfs(graph, neighbor, visited)
+            self.traverse(graph, neighbor, visited)
         return True
-        
-
