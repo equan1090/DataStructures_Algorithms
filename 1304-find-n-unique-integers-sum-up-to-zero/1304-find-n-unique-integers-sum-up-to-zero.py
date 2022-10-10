722b7119e7a7a67528b26ca448970bd6d1ee50ae
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-10 12:12:30 -0600

    Time: 73 ms (15.33%), Space: 14 MB (17.05%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -1,8 +1,11 @@
 class Solution:
     def sumZero(self, n: int) -> List[int]:
         L, rem = n // 2, n % 2
-        if rem != 0: ans = [0]
-        else: ans = []
+        if rem != 0: 
+            ans = [0]
+        else: 
+            ans = []
+        
         for i in range(1,L+1):
             ans.append(-i)
             ans.append(i) 
