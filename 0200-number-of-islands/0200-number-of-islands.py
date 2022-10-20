5d38d9445781c99a8ca08cbe2a3d09683b047324
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-20 14:02:22 -0600

    Time: 397 ms (79.19%), Space: 21.8 MB (25.05%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if self.dfs(grid, r, c, visited):
+                    count += 1
+        return count
+    
+    def dfs(self,grid, r, c, visited):
+        rowbounds = 0 <= r < len(grid)
+        colbounds = 0 <= c < len(grid[0])
+        
+        if not rowbounds or not colbounds or (r, c) in visited or grid[r][c] == '0':
+            return False
+        
+        visited.add((r, c))
+        
+        self.dfs(grid, r + 1, c, visited)
+        self.dfs(grid, r - 1, c, visited)
+        self.dfs(grid, r, c + 1, visited)
+        self.dfs(grid, r, c - 1, visited)
+        return True
+        
