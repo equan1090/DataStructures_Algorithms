23a71b9adc083ebd8f45a802b49b5656a7d3c258
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-21 11:45:07 -0600

    Time: 66 ms (29.24%), Space: 13.9 MB (12.03%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,9 +1,16 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        for index in range(len(digits)-1, -1, -1):
-            digits[index] += 1
-            if digits[index] != 10:
-                return digits
-            digits[index] = 0
-        return [1] + digits
+        res = []
+        [1, 0, 9]
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += carry
+            if digits[i] > 9:
+                digits[i] %= 10
+            else:
+                carry = 0
+        
+        if carry > 0:
+            return [1] + digits
+        return digits
