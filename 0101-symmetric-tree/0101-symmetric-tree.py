113e0dc5f00c46731602cab42c4f0048d5c11349
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-21 13:50:35 -0600

    Time: 60 ms (58.52%), Space: 13.9 MB (60.82%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -15,7 +15,10 @@ class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if p is None and q is None:
             return True
-        elif p is None or q is None:
+        if p is None or q is None:
             return False
-        else:
-            return p.val == q.val and self.isSameTree(p.left, q.right) and self.isSameTree(p.right, q.left)
+        
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.right) and self.isSameTree(p.right, q.left)
