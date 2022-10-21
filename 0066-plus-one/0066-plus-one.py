d7db96e1133e61292dec34a4946bedc9935d584e
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-21 11:45:34 -0600

    Time: 39 ms (86.09%), Space: 13.8 MB (96.45%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,7 +1,6 @@
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        res = []
         carry = 1
         for i in range(len(digits) - 1, -1, -1):
             digits[i] += carry
