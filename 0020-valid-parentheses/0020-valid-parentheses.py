4fc48d50713c0804b117512c931ebf737fdfd709
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-25 12:35:10 -0600

    Time: 38 ms (85.25%), Space: 13.9 MB (72.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pairs = {
+            '}': '{',
+            ']': '[',
+            ')': '('
+        }
+        
+        stack = []
+        
+        for c in s:
+            if c in pairs.values():
+                stack.append(c)
+            else:
+                if not stack or pairs[c] != stack.pop():
+                    return False
+        return len(stack) == 0
