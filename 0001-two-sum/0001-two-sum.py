349229855a672b193088f891be2d5a4155f96e3e
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-25 12:32:57 -0600

    Time: 102 ms (78.17%), Space: 15.3 MB (25.73%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pairs = {}
+        for i, num in enumerate(nums):
+            compliment = target - num
+            if compliment in pairs:
+                return [i, pairs[compliment]]
+            pairs[num] = i
+        return -1
