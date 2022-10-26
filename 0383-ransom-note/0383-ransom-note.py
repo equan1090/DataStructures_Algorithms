9e1522f7c3dcc9a8c3149d7df4979b2741d70e70
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-26 12:20:05 -0600

    Time: 162 ms (26.37%), Space: 14.2 MB (19.68%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNoteDict = Counter(ransomNote)
-        magazineDict = Counter(magazine)
+        ransomNoteDict = {}
+        magazineDict = {}
+        for c in ransomNote:
+            ransomNoteDict[c] = 1 + ransomNoteDict.get(c, 0)
+        
+        for c in magazine:
+            magazineDict[c] = 1 + magazineDict.get(c, 0)
+        
+        print(ransomNoteDict)
+        print(magazineDict)
+        
         for k in ransomNoteDict:
-            if magazineDict[k] <  ransomNoteDict[k]:
+            if k not in magazineDict or magazineDict[k] < ransomNoteDict[k]:
                 return False
         return True
