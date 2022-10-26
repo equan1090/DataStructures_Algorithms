21b50a32e15f3ff8aa68be9a8509f75d46a48470
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-26 11:44:18 -0600

    Time: 62 ms (20.49%), Space: 13.9 MB (62.77%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -11,8 +11,8 @@ class Solution:
         l = 0
         while(l<=r):
             mid = l + (r-l)//2
-            if isBadVersion(mid)==False:
-                l = mid+1
-            else:
+            if isBadVersion(mid):
                 r = mid-1
+            else:
+                l = mid+1
         return l
