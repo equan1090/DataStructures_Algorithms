8873dd403d933f36f1c7e2ec527b21832ed936f6
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-26 15:24:55 -0600

    Time: 69 ms (34.20%), Space: 14 MB (25.31%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,15 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = list(a)
-        b = list(b)
+        aP = len(a) - 1
+        bP = len(b) - 1
         carry = 0
         res = ''
         
-        while a or b or carry:
-            if a:
-                carry += int(a.pop())
-            if b:
-                carry += int(b.pop())
+        while aP >= 0 or bP >= 0 or carry:
+            if aP >= 0:
+                carry += int(a[aP])
+                aP -= 1
+            if bP >= 0:
+                carry += int(b[bP])
+                bP -= 1
             
             res += str((carry % 2))
             carry //= 2
