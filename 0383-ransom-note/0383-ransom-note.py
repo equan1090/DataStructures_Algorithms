a894d1c6bbf05889b1cf969a32eb59b58f537147
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-10-26 12:20:22 -0600

    Time: 81 ms (74.24%), Space: 14.2 MB (53.52%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -8,9 +8,6 @@ class Solution:
         for c in magazine:
             magazineDict[c] = 1 + magazineDict.get(c, 0)
         
-        print(ransomNoteDict)
-        print(magazineDict)
-        
         for k in ransomNoteDict:
             if k not in magazineDict or magazineDict[k] < ransomNoteDict[k]:
                 return False
