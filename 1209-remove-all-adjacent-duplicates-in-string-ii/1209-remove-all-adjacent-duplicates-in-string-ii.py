ce3890b51b693ce1c8b7097e7925987b0ffa4602
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-11-08 13:29:09 -0700

    Time: 390 ms (24.30%), Space: 18.8 MB (37.56%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -3,13 +3,11 @@ class Solution:
         stack = []
         res = []
         for c in s:
-            if not stack:
+            if not stack or stack[-1][0] != c:
                 stack.append([c, 1])
                 continue
             if stack[-1][0] == c:
                 stack[-1][-1] += 1
-            else:
-                stack.append([c, 1])
             
             if stack and stack[-1][-1] == k:
                 stack.pop()
