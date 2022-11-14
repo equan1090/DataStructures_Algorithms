f10de85bb7765fda01e915fb2c142d38ad9f98d9
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-11-14 10:55:24 -0700

    Time: 50 ms (82.96%), Space: 14.2 MB (14.86%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,23 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str],):
-        
-        
-#         memo = {}
-        
-#         def memoize(target, wordDict):
-#             if target == "":
-#                 return True
-#             if target in memo:
-#                 return memo[target]
-            
-#             for word in wordDict:
-#                 if target[:len(word)] == word and memoize(target[len(word):], wordDict):
-#                     memo[target] = True
-#                     return memo[target]
-#             memo[target] = False
-#             return memo[target]
-        
-#         return memoize(s, wordDict)
+
         return self.dfs(s, wordDict, {})
 
     def dfs(self, s, wordDict, memo):
