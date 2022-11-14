f6bbf165f6d22cf63af91e23ac216546ebf73bfa
Eric Quan <76127850+equan1090@users.noreply.github.com>
2022-11-14 10:52:50 -0700

    Time: 62 ms (74.10%), Space: 14 MB (70.32%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,12 +1,36 @@
 class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s) + 1)
-        dp[len(s)] = True
+    def wordBreak(self, s: str, wordDict: List[str],):
         
-        for i in range(len(s) -1, -1, -1):
-            for w in wordDict:
-                if (i + len(w)) <= len(s) and s[i:i + len(w)] == w:
-                    dp[i] = dp[i + len(w)]
-                if dp[i]:
-                    break
-        return dp[0]
+        
+#         memo = {}
+        
+#         def memoize(target, wordDict):
+#             if target == "":
+#                 return True
+#             if target in memo:
+#                 return memo[target]
+            
+#             for word in wordDict:
+#                 if target[:len(word)] == word and memoize(target[len(word):], wordDict):
+#                     memo[target] = True
+#                     return memo[target]
+#             memo[target] = False
+#             return memo[target]
+        
+#         return memoize(s, wordDict)
+        return self.dfs(s, wordDict, {})
+
+    def dfs(self, s, wordDict, memo):
+        if s in memo:
+            return memo[s]
+        
+        if not s:
+            return True
+        
+        for word in wordDict:
+            if s[:len(word)] == word and self.dfs(s[len(word):], wordDict, memo):
+                memo[s] = True
+                return memo[s]
+        memo[s] = False
+        return memo[s]
+        
