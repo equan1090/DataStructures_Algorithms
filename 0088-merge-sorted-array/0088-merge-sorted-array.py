f713af22b3655001219197ad1febf188004e5d3a
Eric Quan <76127850+equan1090@users.noreply.github.com>
2023-04-21 17:18:49 -0600

    Time: 38 ms (64.58%), Space: 13.9 MB (24.36%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,18 +1,25 @@
-
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        # Initialize nums1's index
-        i = m - 1
-        # Initialize nums2's index
-        j = n - 1
-        # Initialize a variable k to store the last index of the 1st array...
-        k = m + n - 1
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                k -= 1
-                i -= 1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        #Get last index of nums 1
+        last = m + n - 1
+        
+        #Merge in reverse order
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
             else:
-                nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1
+        
+        
+        
+        #fill nums1 with leftover num2 elements
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+            last -= 1
