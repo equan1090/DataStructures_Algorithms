3c12f0c54cf1089a7caf79a83acd3da741e66ec1
Eric Quan <76127850+equan1090@users.noreply.github.com>
2023-04-21 17:21:47 -0600

    Time: 38 ms (64.58%), Space: 13.9 MB (71.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,10 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        #Get last index of nums 1
-        last = m + n - 1
         
-        #Merge in reverse order
+        last = m + n - 1
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
@@ -17,9 +15,8 @@ class Solution:
             last -= 1
         
         
-        
-        #fill nums1 with leftover num2 elements
         while n > 0:
             nums1[last] = nums2[n - 1]
             n -= 1
             last -= 1
+            
