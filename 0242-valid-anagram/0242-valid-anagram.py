a44f2b51be4284c2b93a3a6c66d6d4f2d8c0bbe4
Eric Quan <76127850+equan1090@users.noreply.github.com>
2023-08-11 11:42:28 -0600

    Time: 50 ms (92.05%), Space: 16.7 MB (99.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        return self.anagram(s) == self.anagram(t)
-        
-        
-    def anagram(self, word):
+        return self.counter(s) == self.counter(t)
+    
+    def counter(self, word):
         count = {}
-        for char in word:
-            count[char] = 1 + count.get(char, 0)
+        for c in word:
+            count[c] = 1 + count.get(c, 0)
         return count
