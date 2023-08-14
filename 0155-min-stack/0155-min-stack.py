33225138818db529b9e21dcee7dec87631959c12
Eric Quan <76127850+equan1090@users.noreply.github.com>
2023-08-14 14:50:42 -0600

    Time: 55 ms (95.98%), Space: 20.5 MB (48.23%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,21 +1,19 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []
         self.mins = []
+        self.stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        
         if not self.mins or self.mins[-1] > val:
             self.mins.append(val)
         else:
             self.mins.append(self.mins[-1])
 
     def pop(self) -> None:
-        self.stack.pop()
         self.mins.pop()
-        
+        self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
