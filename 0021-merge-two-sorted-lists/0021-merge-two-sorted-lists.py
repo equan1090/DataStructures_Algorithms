994011661f6068326f0c11ae5460bb0573a9d1b2
Eric Quan <76127850+equan1090@users.noreply.github.com>
2023-08-14 15:42:22 -0600

    Time: 42 ms (86.90%), Space: 16.2 MB (81.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -18,8 +18,5 @@ class Solution:
                 l2 = l2.next
             tail = tail.next
         
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
+        tail.next = l1 or l2
         return dummy.next
